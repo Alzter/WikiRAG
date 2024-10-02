a6f635d2fcbf212ae958802f20d722743c5578da
Alzter <alexsmudgy20@gmail.com>
2024-10-03 01:07:28 +1000

    Refactor Wikipedia corpus download notebook into 2 python scripts

---
+++ corpus-embedding.py
@@ -0,0 +1,151 @@
+
+import json, os
+import torch
+from transformers import AutoModel, AutoTokenizer, BitsAndBytesConfig
+
+class CorpusEmbedding():
+
+    def __init__(self, model_name = "jinaai/jina-embeddings-v2-base-en", quantized = True):
+        """
+        Create the model and tokenizer needed.
+        """
+        # Run the device on GPU only if NVIDIA CUDA drivers are installed.
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=quantized,
+            bnb_4bit_compute_dtype=torch.bfloat16 if quantized else None
+        )
+
+        self.model = AutoModel.from_pretrained(model_name, trust_remote_code=True, device_map='cuda', quantization_config = quantization_config)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, device_map='cuda', quantization_config = quantization_config)
+
+    def chunk_by_sentences(raw_text_corpus_path: str, tokenizer: callable):
+        """
+        Split the input text into sentences using the tokenizer.
+        
+        Args:
+            raw_text_corpus_path (str): The path of the raw text corpus to split into sentences.
+            param tokenizer: The tokenizer to use.
+        
+        Returns:
+            chunks (list): The list of text chunks.
+            span_annotations (list): The location for each text chunk within the corpus.
+        """
+
+        inputs = tokenizer(raw_text_corpus_path, return_tensors='pt', return_offsets_mapping=True)
+        punctuation_mark_id = tokenizer.convert_tokens_to_ids('.')
+        sep_id = tokenizer.convert_tokens_to_ids('[SEP]')
+        token_offsets = inputs['offset_mapping'][0]
+        token_ids = inputs['input_ids'][0]
+        chunk_positions = [
+            (i, int(start + 1))
+            for i, (token_id, (start, end)) in enumerate(zip(token_ids, token_offsets))
+            if token_id == punctuation_mark_id
+            and (
+                token_offsets[i + 1][0] - token_offsets[i][1] > 0
+                or token_ids[i + 1] == sep_id
+            )
+        ]
+        chunks = [
+            raw_text_corpus_path[x[1] : y[1]]
+            for x, y in zip([(1, 0)] + chunk_positions[:-1], chunk_positions)
+        ]
+        span_annotations = [
+            (x[0], y[0]) for (x, y) in zip([(1, 0)] + chunk_positions[:-1], chunk_positions)
+        ]
+        return chunks, span_annotations
+
+    def read_input_texts_from_folder(self, raw_text_corpus_path):
+        """
+        Read text content from all JSON files in a folder and concatenates it into a single string.
+        
+        Args:
+            raw_text_corpus_path (str): The path of the raw text corpus to read.
+        
+        Returns:
+            all_text (str): All text from the folder concatenated using line breaks.
+        """
+
+        all_text = ""
+        for root, _, files in os.walk(raw_text_corpus_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    for line in f:
+                        try:
+                            # Parse each line as JSON and extract the 'text' field
+                            data = json.loads(line)
+                            text_content = data.get('text', '').strip()  # Strip any leading/trailing whitespace
+                            if text_content:  # Ensure only non-empty content is added
+                                all_text += text_content + "\n"
+                        except json.JSONDecodeError:
+                            continue  # Skip lines that are not valid JSON
+        return all_text
+
+    def late_chunking(self, model_output: "BatchEncoding", span_annotation: list, max_length=None):
+        """
+        Performs late chunking on a list of embeddings.
+
+        Args:
+            model_output (list): A list of embeddings to chunk.
+            span_annotation (list): A list of chunk locations to use where each location is a tuple.
+            max_length (int, optional): The maximum permitted length of chunks.
+        
+        Returns:
+            outputs (list): The embeddings pooled into chunks.
+        """
+        token_embeddings = model_output[0]
+        outputs = []
+        for embeddings, annotations in zip(token_embeddings, span_annotation):
+            if (
+                max_length is not None
+            ):  # remove annotations which go bejond the max-length of the model
+                annotations = [
+                    (start, min(end, max_length - 1))
+                    for (start, end) in annotations
+                    if start < (max_length - 1)
+                ]
+            pooled_embeddings = [
+                embeddings[start:end].sum(dim=0) / (end - start)
+                for start, end in annotations
+                if (end - start) >= 1
+            ]
+            pooled_embeddings = [
+                embedding.detach().cpu().numpy() for embedding in pooled_embeddings
+            ]
+            outputs.append(pooled_embeddings)
+
+        return outputs
+
+    def corpus_to_embeddings(self, raw_text_corpus_path : str, use_late_chunking = True):
+        """
+        Converts a raw text knowledge corpus into a series of chunked embeddings.
+
+        Args:
+            raw_text_corpus_path (str): The path of the raw text corpus to read.
+            use_late_chunking (bool): If true, performs text embedding *before* text chunking. This is a technique known as [late chunking](https://arxiv.org/abs/2409.04701), which significantly improves semantic meaning of embeddings.
+        
+        Returns:
+            embeddings (list): A list of chunk embeddings from the knowledge base.
+        """
+
+        # Read the entire corpus as a single string.
+        input_text = self.read_input_texts_from_folder(raw_text_corpus_path)
+
+        chunks, span_annotations = self.chunk_by_sentences(input_text, self.tokenizer)
+
+        # Print the combined extracted text (for debugging purposes)
+        # if input_text.strip():
+        #     print(f"Extracted Text: {input_text[:1000]}...")  # Print first 1000 characters to check
+        # else:
+        #     print("No text extracted")
+
+        if use_late_chunking:
+            inputs = self.tokenizer(input_text, return_tensors='pt').to(self.device)
+            model_output = self.model(**inputs)
+            embeddings = self.late_chunking(model_output, [span_annotations])[0]
+        else:
+            embeddings = self.model.encode(chunks)
+        
+        return embeddings
---
+++ wikipedia-corpus-download.py
@@ -0,0 +1,91 @@
+import requests
+import os, subprocess, json
+import sys; sys.path.append("wiki_extractor")
+import WikiExtractor
+
+class WikipediaDownload():
+    """
+    Downloads all of Wikipedia and converts it to raw text using [WikiExtractor](https://github.com/attardi/wikiextractor).
+    """
+
+    def download_wikipedia(dump_url, download_subset, 
+                        dump_file_path='dumps/wikipedia_dump_file.bz2'):
+        """
+        Downloads the full Wikipedia dump or a 5 MB subset.
+
+        Args:
+            dump_url (str): The URL of the Wikipedia dump file to be downloaded.
+            download_subset (bool): If true, only downloads a 5 MB subset of the Wikipedia dump.
+            dump_file_path (str, optional): Path where the downloaded file will be
+            saved (default: 'wikipedia_dump_file.bz2').
+        
+        Returns:
+            path (str): The path to the saved dump file.
+        """
+
+        dump_folder = os.path.split(dump_file_path)[0]
+        if not os.path.exists(dump_folder): os.makedirs(dump_folder)
+
+        # Stream the file download based on user's choice (0 = full, 1 = subset)
+        with requests.get(dump_url, stream=True) as r, open(dump_file_path, 'wb') as f:
+            r.raise_for_status()  # Raise an error for bad responses
+            
+            # Download 5 MB subset
+            if download_subset:
+                downloaded_size = 0
+                for chunk in r.iter_content(1024):  # Download data in chunks of 1 KB
+                    if chunk:
+                        f.write(chunk)  # Write the chunk to the file
+                        downloaded_size += len(chunk)  # Increase size counter
+                        if downloaded_size >= 5_000_000:  # Stop after 5 MB
+                            break
+            
+            # Download the full dump
+            else:
+                for chunk in r.iter_content(1024):
+                    if chunk:
+                        f.write(chunk)
+                print(f"Downloaded full dump.")
+        
+        print(f"Dump saved to {dump_file_path}")
+        return dump_file_path
+
+    def extract_wikipedia_dump(dump_file_path, output_dir='wikipedia_extracted', is_subset=True, use_local_wikiextractor = True):
+        """
+        Extracts plain text from the Wikipedia dump using WikiExtractor.
+
+        Args:
+            dump_file_path (str): The path to the downloaded Wikipedia dump file.
+            output_dir (str, optional): The directory where the extracted text will be saved.
+                                        Defaults to 'wikipedia_extracted'.
+            is_subset (bool, optional): If True, assume the dump file is a small subset (e.g., 5MB).
+                                        If False, process the full dump. Defaults to True.
+            use_local_wikiextractor (bool, optional):
+
+        
+        Returns:
+            None
+        """
+
+        #  Create output directory if it does not exist
+        os.makedirs(output_dir, exist_ok=True)
+
+        if use_local_wikiextractor:
+            if is_subset:
+                WikiExtractor.main(input=dump_file_path, json=True, no_templates=True, output=output_dir, bytes="5M")
+            else:
+                WikiExtractor.main(input=dump_file_path, json=True, no_templates=True, output=output_dir)
+        
+        else:
+            
+            # Use '--bytes' for smaller subsets
+            extractor_command = ['wikiextractor', '--json', '--no-templates', '-o', output_dir, dump_file_path]
+
+            if is_subset:
+                extractor_command.insert(1, '--bytes')
+                extractor_command.insert(2, '5M')  #5  MB
+
+            # Run wikiextractor via subprocess
+            subprocess.run(extractor_command, check=True)
+
+        print(f"Extraction completed.  Extracted files are saved in {output_dir}")
