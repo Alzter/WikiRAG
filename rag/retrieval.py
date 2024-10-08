d0ce317ebb3ec334fcd61f62df66a271579c4926
Alzter <alexsmudgy20@gmail.com>
2024-10-09 00:15:27 +1100

    Sparse and dense retrieval classes

---
+++ rag/retrieval.py
@@ -0,0 +1,8 @@
+class Document():
+    def __init__(self, name : str, summary : str):
+        self.name = name; self.summary = summary
+
+class Embedding():
+    def __init__(self, text : str, vector):
+        self.text = text; self.vector = vector
+        
