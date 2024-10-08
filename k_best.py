c1b9d5f7ce5f4b15c827b037b1ba38322c7f2dde
Alzter <alexsmudgy20@gmail.com>
2024-10-08 22:53:56 +1100

    Class to get k_best results

---
+++ k_best.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def get_k_best(k : int, items : list[object], scores : list[float]):
+    """
+    Given an array of items and a corresponding array of scalar item scores,
+    return k items which have the highest corresponding scores in descending order.
+
+    E.g., for item array ['apple', 'banana', 'orange', 'grapefruit', 'mango'] with scores [0.8, 0.1, -0.5, 0.2, 0.75] and k = 3,
+          this function would return ['apple', 'mango', 'grapefruit'], because these items have the greatest scores.
+    
+    Args:
+        k (int): How many items to return.
+        items (list): A list of items, which can be any value.
+        scores (list[float]): The scores for every item. Items with the greatest score are selected first.
+    
+    Returns:
+        best_items (list): The list of k best items according to the highest scores.
+    """
+    assert(len(items) == len(scores), "Each score must correspond to one item.")
+
+    # 0 < k <= len(items)
+    k = min(k, len(items))
+    k = max(k, 0)
+
+    # Get the indices of the highest k elements in descending order
+
+    best_indices = np.argsort(scores)[-k:][::-1]
+
+    best_items = [items[i] for i in best_indices]
+
+    return best_items
