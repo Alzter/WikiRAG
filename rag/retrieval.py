4d710c624f4ac65b0d62ff33fa7605c998129acf
Alzter <alexsmudgy20@gmail.com>
2024-10-09 00:48:40 +1100

    Add method to retrieve embeddings for a document

--- rag/retrieval.py
+++ rag/retrieval.py
@@ -1,5 +1,5 @@
-
 import glob, os
+import numpy as np
 
 class Document():
     def __init__(self, title : str, summary : str):
@@ -14,6 +14,7 @@ class Retrieval():
     
     def __init__(self, corpus_path : str):
         self.documents = self.get_document_summaries(corpus_path)
+        self.corpus_path = corpus_path
 
     def get_document_summaries(self, corpus_path : str):
 
@@ -31,8 +32,6 @@ class Retrieval():
             # Get the parent directory of the summary file
             parent_directory = os.path.split(os.path.dirname(summary_file))[-1]
 
-            print(summary_file)
-
             with open(summary_file, "r") as f:
                 summary_text = f.read()
 
@@ -42,3 +41,27 @@ class Retrieval():
     
         return documents
 
+    def get_document_embeddings(self, document_name):
+        document_embedding_path = os.path.join(self.corpus_path, document_name.strip())
+
+        embeddings = []
+
+        embedding_files = glob.glob(document_embedding_path + "//chunk_*.npy")
+        embedding_texts = glob.glob(document_embedding_path + "//chunk_*.txt")
+        
+        assert(len(embedding_files) == len(embedding_texts), "Embedding raw text files should directly map to embedding data files.")
+
+        for embedding_file, raw_text_file in zip(embedding_files, embedding_texts):
+            embedding_data = np.load(embedding_file, encoding='bytes', allow_pickle=True)
+
+            with open(raw_text_file, "r") as f:
+                raw_text = f.read()
+            
+            embedding = Embedding(text=raw_text, vector=embedding_data)
+
+            embeddings.append(embedding)
+        
+        return embeddings
+
+            
+            
