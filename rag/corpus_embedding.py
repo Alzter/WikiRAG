579666426fee6bd4bcc59e8103982bc4df595f95
Alzter <alexsmudgy20@gmail.com>
2024-10-10 13:08:44 +1100

    fix bug with corpus embedding class

--- rag/corpus_embedding.py
+++ rag/corpus_embedding.py
@@ -6,6 +6,9 @@ from transformers import AutoModel, AutoTokenizer, BitsAndBytesConfig
 import sys; sys.path.append("processor")
 from embedding_model import EmbeddingModel
 
+import unicodedata
+import re
+
 class CorpusEmbedding(EmbeddingModel):
     """
     Class which can convert a raw-text corpus of Wikipedia into an embedding database.
@@ -13,6 +16,25 @@ class CorpusEmbedding(EmbeddingModel):
 
     def __init__(self):
         return super().__init__()
+    
+    def sanitise_string(input_string : str, allow_unicode=False):
+            """
+            Sanitises a string to make it usable as a folder name
+            by removing illegal characters such as apostrophes.
+
+            Taken from https://github.com/django/django/blob/master/django/utils/text.py
+            Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+            dashes to single dashes. Remove characters that aren't alphanumerics,
+            underscores, or hyphens. Convert to lowercase. Also strip leading and
+            trailing whitespace, dashes, and underscores.
+            """
+            value = str(value)
+            if allow_unicode:
+                value = unicodedata.normalize('NFKC', value)
+            else:
+                value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+            value = re.sub(r'[^\w\s-]', '', value.lower())
+            return re.sub(r'[-\s]+', '-', value).strip('-_')
 
     def read_input_texts_from_folder(self, raw_text_corpus_path, return_as_string):
         """
@@ -51,6 +73,7 @@ class CorpusEmbedding(EmbeddingModel):
         articles = []
         
         for article in corpus:
+
             paragraphs = article.split("\n\n")
             try:
                 assert((len(paragraphs)>1), "Article must have more than 2 paragraphs.")
@@ -89,44 +112,53 @@ class CorpusEmbedding(EmbeddingModel):
         if not os.path.exists(output_dir): os.makedirs(output_dir)
 
         for article in articles:
-            title = article['title']
-            summary = article['summary']
-            paragraphs = article['paragraphs']
 
-            article_path = os.path.join(output_dir, title.strip())
-            print(article_path)
-
-            if not os.path.exists(article_path): os.mkdir(article_path)
-
-            summary_file_path = os.path.join(article_path, "summary.txt")
-
-            # Write the summary file.
-            summary_file = open(summary_file_path, "wb")
-            summary_file.write(summary.encode("utf-8"))
-            summary_file.close()
+            try:
             
-            # Embed each summary using the embedding model
-            summary_embedding = self.get_embedding(summary, input_is_query=False)
-            summary_embedding_file_path = os.path.join(article_path, "summary.npy")
+                title = article['title']
+                summary = article['summary']
+                paragraphs = article['paragraphs']
 
-            np.save(summary_embedding_file_path, summary_embedding, allow_pickle=True)
+                folder_name = self.sanitise_string(title.strip())
 
-            # Treat each paragraph as a chunk
-            for paragraph_id, paragraph in enumerate(paragraphs):
+                article_path = os.path.join(output_dir, folder_name)
+                print(f"Embedding article: {article_path}")
+
+                if not os.path.exists(article_path): os.mkdir(article_path)
+
+                summary_file_path = os.path.join(article_path, "summary.txt")
+
+                # Write the summary file.
+                summary_file = open(summary_file_path, "wb")
+                summary_file.write(summary.encode("utf-8"))
+                summary_file.close()
                 
-                # Embed each paragraph using the embedding model
-                embedding = self.get_embedding(paragraph, input_is_query=False)
+                # Embed each summary using the embedding model
+                summary_embedding = self.get_embedding(summary, input_is_query=False)
+                summary_embedding_file_path = os.path.join(article_path, "summary.npy")
 
-                # Save the raw text of the paragraph into a text file
-                paragraph_raw_text_file_path = os.path.join(article_path, f"chunk_{paragraph_id}.txt")
-                paragraph_raw_text_file = open(paragraph_raw_text_file_path, "wb")
-                paragraph_raw_text_file.write(paragraph.encode("utf-8"))
-                paragraph_raw_text_file.close()
+                np.save(summary_embedding_file_path, summary_embedding, allow_pickle=True)
 
-                # Save the embedding of the paragraph into a numpy file
-                embedding_data_file_path = os.path.join(article_path, f"chunk_{paragraph_id}.npy")
-                np.save(embedding_data_file_path, embedding, allow_pickle=True)
+                # Treat each paragraph as a chunk
+                for paragraph_id, paragraph in enumerate(paragraphs):
+                    
+                    # Embed each paragraph using the embedding model
+                    embedding = self.get_embedding(paragraph, input_is_query=False)
 
-                print(f"Embed paragraph {paragraph_id} for article {title}")
+                    # Save the raw text of the paragraph into a text file
+                    paragraph_raw_text_file_path = os.path.join(article_path, f"chunk_{paragraph_id}.txt")
+                    paragraph_raw_text_file = open(paragraph_raw_text_file_path, "wb")
+                    paragraph_raw_text_file.write(paragraph.encode("utf-8"))
+                    paragraph_raw_text_file.close()
+
+                    # Save the embedding of the paragraph into a numpy file
+                    embedding_data_file_path = os.path.join(article_path, f"chunk_{paragraph_id}.npy")
+                    np.save(embedding_data_file_path, embedding, allow_pickle=True)
+
+                    print(f"Embed paragraph {paragraph_id} for article {title}")
+                
+            except Exception as e:
+
+                print(f"Error parsing Wikipedia article. Traceback: {str(e)}")
 
         return output_dir
