1231822c1e6c3ec9b57216bb4fc68394075dbfa7
Alzter <alexsmudgy20@gmail.com>
2024-10-24 16:10:19 +1100

    more ui updates (wip)

--- app_chat.py
+++ app_chat.py
@@ -1,5 +1,5 @@
 import gradio as gr
-from rag.iterative_retrieval import IterativeRetrieval
+# from rag.iterative_retrieval import IterativeRetrieval
 import os
 
 # KB_PATH = "context/sonic" # TOOD: Add a selector interface to create this.
@@ -19,17 +19,29 @@ import os
 def chat(message, history):
     return "Dummy response"
 
-with gr.Blocks(fill_height=True) as ui:
+with gr.Blocks(
+    # Source: https://github.com/zylon-ai/private-gpt/issues/1377
+    title="RAG system",
+    css=".contain { display: flex !important; flex-direction: column !important; }"
+    "#component-0, #component-3, #component-10, #component-8  { height: 100% !important; }"
+    "#chatbot { flex-grow: 1 !important; overflow: auto !important;}"
+    "#col { height: 100vh !important; }"
+) as ui:
 
-    with gr.Row():
-        with gr.Blocks() as settings_ui:
-            upload_btn = gr.UploadButton(label="Upload Context as PDF")
+    with gr.Row(equal_height=True):
+        
+        # Settings Panel
+        with gr.Column(scale=3) as settings_ui:
+            with gr.Blocks():
+                upload_btn = gr.UploadButton(label="Upload Context as PDF")
 
-        chat_ui = gr.ChatInterface(
-            fn = chat,
-            multimodal=False,
-            type='messages',
-            fill_height=True
-        )
+        # Chat Panel
+        with gr.Column(scale=7):
+            _ = gr.ChatInterface(
+                fn = chat,
+                multimodal=False,
+                type='messages',
+                fill_height=True
+            )
 
-chat_ui.launch()
+ui.launch()
