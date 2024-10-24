13e7d89f130a1faaf998362e38115a2699dcef15
Alzter <alexsmudgy20@gmail.com>
2024-10-25 00:14:31 +1100

    state variable shennanigans

--- app_chat.py
+++ app_chat.py
@@ -16,8 +16,27 @@ import os
 #     response, history, contexts = rag.answer_multi_hop_question(message_text, max_new_tokens=100)
 #     return response
 
+# ====================================================================
+# State variables
+state = {}
+def set_state(variable, value):
+    state[variable] = value
+    print(state)
+def get_state(variable): return state.get(variable) if variable in state.keys() else None
+# ====================================================================
+
+# ====================================================================
+# Utility Methods
+
+def upload_pdf_to_kb(pdf_file, event: gr.EventData):
+    file = open(pdf_file.name, 'rb')
+    print(file)
+
 def chat(message, history):
     return "Dummy response"
+# ====================================================================
+
+# UI
 
 with gr.Blocks(
     title='RAG',
@@ -26,10 +45,24 @@ with gr.Blocks(
 ) as ui:
 
     with gr.Row(equal_height=False):
+
         # Settings Panel
-        with gr.Column(scale=3) as settings_ui:
-            with gr.Blocks():
-                upload_btn = gr.UploadButton(label="Upload Context as PDF")
+        with gr.Column(scale=3, elem_id='col') as settings_ui:
+            
+            max_tokens = gr.Slider(10, 1000, label="Max Tokens")
+            max_tokens.change(lambda value : set_state("max_tokens", value), max_tokens)
+
+            use_rag = gr.Checkbox(label="Use RAG")
+            use_rag.select(lambda value : set_state("rag_enabled", value), use_rag)
+
+            upload_btn = gr.UploadButton(label="Upload Context as PDF", file_types=['.pdf'])
+            upload_btn.upload(upload_pdf_to_kb, upload_btn)
+
+            with gr.Accordion("RAG Settings"):
+                
+                kb_path = gr.Textbox(label="Knowledge Base Path", info="Path which contains your Knowledge Bases")
+                kb_path.change(lambda value : set_state("kb_path", value), kb_path)
+                pass
 
         # Chat Panel
         with gr.Column(scale=7, elem_id='col'):
